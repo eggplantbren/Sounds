fdc2c0e02ba74bbc49d52f803316585ca0c6646a
Brendon J. Brewer <brendon.brewer@gmail.com>
2013-06-09 19:11:22 +1200

    Can't get away from organs

--- main.py
+++ main.py
@@ -4,15 +4,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 harmonics = np.arange(1, 50)
-amplitudes = np.exp(-harmonics/5.)
+amplitudes = np.exp(-(harmonics-2)**2/2.**2)
 
-s = Sound(440., amplitudes)
-s.apply_formant(Formant([2000.], [1.], [500.]))
-s.make_wave_file()
+# Supposedly the I vowel (en.wikipedia.org/wiki/Formant)
+EH = Formant([240., 2400.], [1., 1.], [500., 500.])
 
-plt.plot(harmonics, s.amplitudes, 'bo-')
+# Make a middle C
+s = Sound(220.*2.**(3./12), amplitudes)
+
+plt.plot(harmonics, s.amplitudes, 'bo-', label='Input Source')
 plt.xlabel('Harmonic')
 plt.ylabel('Amplitude')
-plt.title('Input Source')
+
+# Apply the EH
+s.apply_formant(EH)
+s.make_wave_file()
+
+plt.plot(harmonics, s.amplitudes, 'ro-', label='With Formant')
+plt.legend()
 plt.show()
 
