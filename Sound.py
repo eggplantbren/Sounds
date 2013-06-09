d1a293cff79d08b9b52a80acc2ded52202fc7a28
Brendon J. Brewer <brendon.brewer@gmail.com>
2013-06-09 18:48:43 +1200

    Can apply a formant

--- Sound.py
+++ Sound.py
@@ -7,22 +7,31 @@ Yeah, I could use FFTs and stuff. Possibly at the expense of clarity
 and I don't need efficiency at this point.
 """
 
+class Formant:
+	"""
+	Mixture of Gaussians model for formant structure
+	"""
+	def __init__(self, frequencies, amplitudes, widths):
+		assert len(frequencies) == len(amplitudes)
+		assert len(frequencies) == len(widths)
+		self.frequencies = frequencies
+		self.amplitudes = amplitudes
+		self.widths = widths
+
+	def evaluate(self, f):
+		"""
+		Evaluate the formant function at some frequencies
+		"""
+		y = np.zeros(len(f))
+		for i in xrange(0, len(self.frequencies)):
+			y += self.amplitudes[i]*\
+				np.exp(-0.5*((f - self.frequencies[i])/self.widths[i])**2)
+		return y
+
+
 class Sound:
 	"""
-	Models a vocal sound. Input: source frequency spectrum
-	(e.g. vocal fold raw output) and a filter object (Formant)
-	"""
-	def __init__(self, source, filt):
-		pass
-
-
-class Formant:
-	pass
-
-
-class Source:
-	"""
-	A raw vocal sound
+	A sound
 	"""
 	def __init__(self, fundamental,	amplitudes=np.array([1., 0., 0.])):
 		self.fundamental = fundamental
@@ -41,4 +50,12 @@ class Source:
 		scaled = np.int16(data/np.max(np.abs(data))*32767)
 		write(filename, bitrate, scaled)
 		return scaled
+
+	def apply_formant(self, formant):
+		"""
+		Modify the amplitudes according to the formant given
+		"""
+		f = self.fundamental*np.arange(1, len(self.amplitudes) + 1)
+		y = formant.evaluate(f)
+		self.amplitudes *= y
 
--- main.py
+++ main.py
@@ -6,10 +6,11 @@ import matplotlib.pyplot as plt
 harmonics = np.arange(1, 50)
 amplitudes = np.exp(-harmonics/5.)
 
-s = Source(440., amplitudes)
+s = Sound(440., amplitudes)
+s.apply_formant(Formant([2000.], [1.], [500.]))
 s.make_wave_file()
 
-plt.plot(harmonics, amplitudes, 'bo-')
+plt.plot(harmonics, s.amplitudes, 'bo-')
 plt.xlabel('Harmonic')
 plt.ylabel('Amplitude')
 plt.title('Input Source')
